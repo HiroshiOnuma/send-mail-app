9e9d6af6e87726d6a93131ede00e283c4c21cb06
oonumahiroshi <honuma596@gmail.com>
2025-04-20 22:47:22 +0900

    Java Mail、setting/mail.properties追加

--- src/controller/RecipientServlet.java
+++ src/controller/RecipientServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.annotation.*;
 
 import model.*;
 
-@WebServlet({ "/recipient-form", "/recipients", "/recipient-detail", "/recipient-edit" })
+@WebServlet({ "/recipient-form", "/recipients", "/recipient-detail", "/recipient-edit", "/recipient-mail-setting" })
 
 public class RecipientServlet extends HttpServlet {
 
@@ -27,23 +27,32 @@ public class RecipientServlet extends HttpServlet {
 
             // 個別案件ページへのGETリクエスト
         } else if ("/recipient-detail".equals(path)) {
-            Recipient recipient = ServletHelper.fetchEntityOrRedirect(req, res, "recipientId", 
+            Recipient recipient = ServletHelper.fetchEntityOrRedirect(req, res, "recipientId",
                     id -> new RecipientDAO().getRecipientById(id), "/recipients");
 
-                    if(recipient != null) {
-                        req.setAttribute("recipient", recipient);
-                        req.getRequestDispatcher("recipient-detail.jsp").forward(req, res);
-                    }
+            if (recipient != null) {
+                req.setAttribute("recipient", recipient);
+                req.getRequestDispatcher("recipient-detail.jsp").forward(req, res);
+            }
 
             // 配信先編集ページへのGETリクエスト
-        } else if("/recipient-edit".equals(path)) {
-            Recipient recipient = ServletHelper.fetchEntityOrRedirect(req, res, "recipientId", 
+        } else if ("/recipient-edit".equals(path)) {
+            Recipient recipient = ServletHelper.fetchEntityOrRedirect(req, res, "recipientId",
                     id -> new RecipientDAO().getRecipientById(id), "/recipient-edit");
 
-                    if(recipient != null) {
-                        req.setAttribute("recipient", recipient);
-                        req.getRequestDispatcher("recipient-edit.jsp").forward(req, res);
-                    }
+            if (recipient != null) {
+                req.setAttribute("recipient", recipient);
+                req.getRequestDispatcher("recipient-edit.jsp").forward(req, res);
+            }
+            // 配信先メール設定ページへのGETリクエスト
+        } else if ("/recipient-mail-setting".equals(path)) {
+            Recipient recipient = ServletHelper.fetchEntityOrRedirect(req, res, "recipientId",
+                    id -> new RecipientDAO().getRecipientById(id), "/recipient-mail-setting");
+
+            if (recipient != null) {
+                req.setAttribute("recipient", recipient);
+                req.getRequestDispatcher("recipient-mail-setting.jsp").forward(req, res);
+            }
         }
     }
 
@@ -79,16 +88,18 @@ public class RecipientServlet extends HttpServlet {
             req.setAttribute("recipientEmailFormatError", recipientEmailFormatError);
             req.getRequestDispatcher("recipient-register.jsp").forward(req, res);
             return;
-        } 
+        }
 
         // 配信先更新処理
-        if("update".equals(action)) {
-            ServletHelper.processEntityOperation(req, res, "recipientId", "recipients", 
-                    (int id) -> recipientDao.updateRecipientById(id, recipientName, recipientEmail), "recipients", "recipient-detail.jsp", "案件更新処理中にエラーが発生しました。");
+        if ("update".equals(action)) {
+            ServletHelper.processEntityOperation(req, res, "recipientId", "recipients",
+                    (int id) -> recipientDao.updateRecipientById(id, recipientName, recipientEmail), "recipients",
+                    "recipient-detail.jsp", "案件更新処理中にエラーが発生しました。");
             return;
         }
-        
+
         // 配信先登録処理
-        ServletHelper.InsertEntity(req, res, () -> recipientDao.recipientInsert(recipientName, recipientEmail), "dashboard.jsp", "recipient-register.jsp", "配信先登録処理中にエラーが発生しました。");
+        ServletHelper.InsertEntity(req, res, () -> recipientDao.recipientInsert(recipientName, recipientEmail),
+                "dashboard.jsp", "recipient-register.jsp", "配信先登録処理中にエラーが発生しました。");
     }
 }
